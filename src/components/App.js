c400d5979d5926a994bcb577f217fb82da94c7c1
Osama Yousafzai <osama.yousafzai@gmail.com>
2018-11-22 15:10:45 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
+import "./App.css";
+import AgeStats from "./AgeStats";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: "",
+      birthday: "1975-06-12",
+      showStats: false
+    };
+  }
+
+  changeBirthday() {
+    console.log("changeBirthday():", this.state);
+    this.setState({
+      birthday: this.state.newDate,
+      showStats: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form inline>
+          <h2>Input Your Birthday!</h2>
+          <FormControl
+            type="date"
+            onChange={event => this.setState({ newDate: event.target.value })}
+          />
+          {""}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
+
+          {this.state.showStats ? (
+            <div className="fade age-stats">
+              <AgeStats date={this.state.birthday} />
+            </div>
+          ) : (
+            <div />
+          )}
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default App;
