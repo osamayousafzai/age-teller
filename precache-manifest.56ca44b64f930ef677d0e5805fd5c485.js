365bcbca334fd9ba6fd5d28ac4bdecd3ac292351
Osama Yousafzai <osama.yousafzai@gmail.com>
2018-11-22 15:21:15 +0200

    Updates

---
+++ precache-manifest.56ca44b64f930ef677d0e5805fd5c485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4580939b3bb7bd28d9dc34bb0e395f4c",
+    "url": "/age-teller/static/media/party-popper.4580939b.jpg"
+  },
+  {
+    "revision": "d01887d9499b5165fa26",
+    "url": "/age-teller/static/js/runtime~main.d01887d9.js"
+  },
+  {
+    "revision": "43c5a69d06341fe2c908",
+    "url": "/age-teller/static/js/main.43c5a69d.chunk.js"
+  },
+  {
+    "revision": "a344badb5bd8fc37fb45",
+    "url": "/age-teller/static/js/1.a344badb.chunk.js"
+  },
+  {
+    "revision": "43c5a69d06341fe2c908",
+    "url": "/age-teller/static/css/main.5c0bebe3.chunk.css"
+  },
+  {
+    "revision": "e19d11d2b6fb9383dc89662674aabd7e",
+    "url": "/age-teller/index.html"
+  }
+];
